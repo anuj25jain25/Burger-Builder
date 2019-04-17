b27ea41bee86f6807284d7276c047ebfec8b36c1
Anuj Jain <anuj25jain25@gmail.com>
2019-04-18 00:42:40 +0530

    toolbar implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Layout from "./components/layout/layout";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Layout>
+        <p>test</p>
+      </Layout>
     );
   }
 }
